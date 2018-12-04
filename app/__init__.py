1fbd6adfd19460f2152bc529e30829a904d479f1
zemags <azmt2008@gmail.com>
2018-12-04 12:54:20 +0300

    added db, new models and url_for()

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app) # database
+migrate = Migrate(app, db) # migration
 
-from app import routes
+from app import routes, models #models - structure of db
