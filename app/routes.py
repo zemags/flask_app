1cd5b68241d82419f9a60254a03dc23f2be71722
zemags <azmt2008@gmail.com>
2018-12-04 10:48:12 +0300

    Initiate commit

---
+++ app/routes.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from flask import render_template, flash, redirect
+
+from app import app
+from app.forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'aloha'}
+    # creating mock objects
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
